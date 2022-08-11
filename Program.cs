8d96083983648c2a0443beee91ec807cfdd0d9af
abhi-chanale <109659976+abhi-chanale@users.noreply.github.com>
2022-08-11 14:30:04 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -191,7 +191,7 @@ namespace Application
 //######## Compile Time Polymorphism ########//
 
 
-
+/*
 using System;
 class Staff
 {
@@ -220,3 +220,65 @@ class AllEmp
     }
 }
 
+*/
+
+       //######### hierarchical Inheritance ########//
+
+class Teacher
+{
+
+	public void programming()
+	{
+		Console.WriteLine("Java programming...");
+	}
+
+	public void physics()
+	{
+
+		Console.WriteLine("Physics...");
+	}
+
+	public void chemistry()
+	{
+
+		Console.WriteLine("Chemistry...");
+
+	}
+}
+
+ class ComputerDepartment : Teacher
+{
+
+	public void learn()
+	{
+		Console.WriteLine("ComputerDepartment : Learn...");
+	}
+
+}
+
+class ScienceDepartment : Teacher
+{
+
+	public void learn()
+	{
+		Console.WriteLine("\nScienceDepartment : Learn...");
+	}
+}
+
+
+public class TestHierarchicalInheritance
+{
+
+	public static void Main(String[] args)
+	{
+
+		ComputerDepartment cd = new ComputerDepartment();
+		cd.learn();
+		cd.programming();
+
+		ScienceDepartment sd = new ScienceDepartment();
+		sd.learn();
+		sd.physics();
+		sd.chemistry();
+}
+}
